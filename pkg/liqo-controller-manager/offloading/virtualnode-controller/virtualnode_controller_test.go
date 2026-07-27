6079c373fb62b1f4cfa98f7e5472c1cf13243db3
Tadeuš Varnas <varnastadeus@gmail.com>
2026-07-27 15:13:51 +0300

    fix: use vk webhook auth (#68)

--- pkg/liqo-controller-manager/offloading/virtualnode-controller/virtualnode_controller_test.go
+++ pkg/liqo-controller-manager/offloading/virtualnode-controller/virtualnode_controller_test.go
@@ -22,6 +22,7 @@ import (
 	. "github.com/onsi/gomega"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	rbacv1 "k8s.io/api/rbac/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
@@ -293,6 +294,28 @@ var _ = Describe("VirtualNode controller", func() {
 				return controllerutil.ContainsFinalizer(virtualNode1, virtualNodeControllerFinalizer)
 			}, timeout, interval).Should(BeTrue())
 
+		})
+
+		It(fmt.Sprintf("Check if the auth-delegator ClusterRoleBinding is created for %s", nameVirtualNode1), func() {
+			vkName := vkforge.VirtualKubeletName(virtualNode1)
+			expectedCRB := vkforge.VirtualKubeletAuthDelegatorClusterRoleBinding(tenantNamespace1.Name, vkName, remoteClusterID1)
+
+			By(fmt.Sprintf("Try to get the auth-delegator ClusterRoleBinding: %s", expectedCRB.Name))
+			Eventually(func() bool {
+				var crb rbacv1.ClusterRoleBinding
+				if err := k8sClient.Get(ctx, types.NamespacedName{Name: expectedCRB.Name}, &crb); err != nil {
+					return false
+				}
+				// Verify the binding points to the built-in system:auth-delegator role.
+				if crb.RoleRef.Name != "system:auth-delegator" || crb.RoleRef.Kind != "ClusterRole" {
+					return false
+				}
+				// Verify the subject is the virtual kubelet service account.
+				if len(crb.Subjects) != 1 || crb.Subjects[0].Name != vkName || crb.Subjects[0].Namespace != tenantNamespace1.Name {
+					return false
+				}
+				return true
+			}, timeout, interval).Should(BeTrue())
 		})
 
 		It(fmt.Sprintf("Check if finalizers are correctly created for %s", nameVirtualNode2), func() {
